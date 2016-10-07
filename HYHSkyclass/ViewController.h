340c676c2b4b6bda942a9d5748d3fa8f5ae16f2d
Morgan-1 <d274798495@gmail.com>
2016-10-07 16:35:20 +0800

    Initial Commit

---
+++ HYHSkyclass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYHSkyclass
+//
+//  Created by yunhuihuang on 2016/10/7.
+//  Copyright © 2016年 yhhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
